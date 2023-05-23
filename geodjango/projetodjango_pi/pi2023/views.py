a19123d499c4308d7c17b78a56411bedf74fb1f3
camilamuemura <134243688+camilamuemura@users.noreply.github.com>
2023-05-23 20:19:53 -0300

    Update views.py

--- geodjango/projetodjango_pi/pi2023/views.py
+++ geodjango/projetodjango_pi/pi2023/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 
 #Início do exemplo
 
-from .models import Localizacao  
-
-def exibir_localizacao(request):
-    localizacoes = Localizacao.objects.all()
-    return render(request, 'exibir_localizacao.html', {'localizacoes': localizacoes})
-
-def index_example(request):
-    return render(request, 'index_example.html')
+def index_exemplo(request):
+    return render(request, 'pi2023/templates/index_exemplo.html')
 
 # Fim exemplo
