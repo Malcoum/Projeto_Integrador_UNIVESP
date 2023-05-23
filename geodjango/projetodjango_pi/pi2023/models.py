474c4386db58b1f8beda94b413320bb683a40a9c
camilamuemura <134243688+camilamuemura@users.noreply.github.com>
2023-05-23 20:19:05 -0300

    Update models.py

--- geodjango/projetodjango_pi/pi2023/models.py
+++ geodjango/projetodjango_pi/pi2023/models.py
@@ -2,13 +2,13 @@
 
 from django.contrib.gis.db import models
 
-class Localizacao(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    nome = models.CharField(max_length=255)
+##class Localizacao(models.Model):
+##    latitude = models.FloatField()
+##    longitude = models.FloatField()
+##   nome = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.nome
+##   def __str__(self):
+##       return self.nome
 
 
 # Fim do modelo de teste
