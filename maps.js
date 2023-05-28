9ae0d0bb17dd1bddcd8b6379b4e0d87a30513291
jehfuger <31390301+jehfuger@users.noreply.github.com>
2023-05-28 19:06:51 -0300

    Add files via upload

--- maps.js
+++ maps.js
@@ -1,7 +1,3 @@
-
-
-
-
 var map = L.map("map", {zoomSnap:0.01}).setView([-15.83, -47.86], 3);
       var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
@@ -9,13 +5,24 @@ var map = L.map("map", {zoomSnap:0.01}).setView([-15.83, -47.86], 3);
       }).addTo(map);
 	
 	 
-      // http://132.72.155.230:3838/js/geojson-1.html#loading-geojson-files
+      // https://gist.github.com/ricardobeat/674646
 	var geojsonMarkerOptions = {
             radius: 8,
             fillColor: "#ff7800", color: "#000",
             weight: 1, opacity: 1,
             fillOpacity: 0.8
 	};
+
+	var localizationMarkerOptions = {
+		radius: 8,
+		fillColor: "#90EE90", color: "#000",
+		weight: 1, opacity: 1,
+		fillOpacity: 0.8
+    };
+
+
+
+
 // Marcadores representando os pontos de incêndio
 
 	let url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
@@ -36,3 +43,25 @@ var map = L.map("map", {zoomSnap:0.01}).setView([-15.83, -47.86], 3);
 
 			}).addTo(map)
 		});
+
+let h2 = document.querySelector('h2');
+	
+function success(pos){
+	console.log(pos.coords.latitude, pos.coords.longitude);
+		//h2.textContent = `Latitude:${pos.coords.latitude}, Longitude:${pos.coords.longitude}`;
+		L.circleMarker([pos.coords.latitude, pos.coords.longitude],localizationMarkerOptions).addTo(map)
+			.bindPopup('Localização Atual')
+			.openPopup();
+		}
+		
+function error(err){
+		console.log(err);
+		}
+		
+var watchID = navigator.geolocation.watchPosition(success, error, {
+		enableHighAccuracy: true,
+		timeout: 5000
+		});
+		
+//navigator.geolocation.clearWatch(watchID);
+		
